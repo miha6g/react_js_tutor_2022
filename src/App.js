c106fbebebc7afa2bbd9877a954c553d19a687d4
miha6g <72049060+miha6g@users.noreply.github.com>
2022-03-02 12:25:11 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import React, { Component, useState } from 'react';
-import Todo from './components/Todo/Todo';
-
+import TodoList from './components/Todo/Todo';
+import UserList from './components/Users/Users';
+import { Navbar } from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import 
 
 class App extends Component {
 
@@ -9,14 +12,49 @@ class App extends Component {
     title: this.props?.title ? this.props.title : 'Default title'
   }
   
+
   render(){
     const {title} = this.state
     return(
       <>
-        <Todo />
+      <Router>
+        <Navbar>
+          {/* <Todo /> */}
+        </Navbar>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/todo' element={<Todo />}/>
+          <Route path='/users' element={<Users />}/>
+          <Route path='/account' element={<Home />}/>
+        </Routes>
+      </Router>
       </>
     )
   }
   
 }
+
+const Home = ()=> {
+  return(
+    <>
+      <p>Home page</p>
+    </>
+  )
+}
+
+const Todo = ()=> {
+  return(
+    <>
+      <TodoList uid={window.location.hash != '' ? window.location.hash.substring(1) : undefined} />
+    </>
+  )
+}
+const Users = ()=> {
+  return(
+    <>
+      <UserList />
+    </>
+  )
+}
+
 export default App;
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,83 +1,83 @@
 import { Button, Checkbox, List, Col, Input, Pagination } from 'antd';
-import React, {Component } from 'react';
+import React, {useState, useEffect} from 'react';
+import { useFetchData } from '../../hooks/useMyHookTest';
 
-export default class Todo extends Component {
+import classes from './Todo.module.css'
 
-    state = {
-        todoList: [],
-        curentPage : 1,
-        perPage : 10,
-    }
-
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(res => res.json())
-        .then(res => this.setState({todoList : res}))
-        
-    }
-
+function TodoList(props) {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [perPage, setPerPage] = useState(10);
+    const [list, setList] = useState([]);
+    var todos = useFetchData('todos');
     
-    handelerTodoChecked = (todoID) =>{
-        let todo = JSON.parse(JSON.stringify(this.state.todoList))
-
-        todo = todo.filter((item)=>{
-            if(item.id == todoID){
-                item.completed = !item.completed
+    useEffect(() => {
+        setList(todos);
+    }, [todos])
+    
+    function handelerTodoChecked(todoID) {
+        let todo = JSON.parse(JSON.stringify(list));
+        todo = todo.filter((item) => {
+            if (item.id === todoID) {
+                item.completed = !item.completed;
             }
             return item;
         })
-        this.setState({todoList : todo})
+        setList(todo);
     }
 
-    handlerDeleteTodo = (todoID) => {
-        let todo = JSON.parse(JSON.stringify(this.state.todoList))
-
-        todo = todo.filter((item)=>{
-            if(item.id != todoID) return item;
-            
+    function handlerDeleteTodo(todoID) {
+        let todo = JSON.parse(JSON.stringify(list));
+        todo = todo.filter((item) => {
+            if (item.id !== todoID) return item;
+            else return undefined;
         })
-        this.setState({todoList : todo})
+        setList(todo);
     }
 
-    handlerAddToTodo =(e) => {
-        console.log(e)
-        let defVariable = [{id: Math.random(), title:e.target.value, completed: false}]
-        this.setState({todoList : [...this.state.todoList, ...defVariable] })
+    function handlerAddToTodo(e) {
+        console.log(e);
+        let defVariable = [{id: Math.random(), title:e.target.value, completed: false}];
+        setList([...list, ...defVariable]);
     }
 
-    handlerChangePagination = (page, pageSize) => {
-        console.log(page, pageSize)
-        this.setState({perPage : pageSize, curentPage: page})
+    function handlerUpdatePages (page, pageSize) {
+        setCurrentPage(page ? page : 1);
+        setPerPage(pageSize);
     }
 
-    render(){
-        const {todoList} = this.state;
-        const {curentPage} = this.state;
-        const {perPage} = this.state;
-
-        const endList = curentPage*perPage;
-        const beginList = endList - perPage;
-
-        return(
-            <>
-                <Col span={8} style={{margin: '0 auto'}}>
-                    <h3>My todo list</h3>
-                    <List>
-                        <Input placeholder='Добавить todo в список' onPressEnter={(e) => this.handlerAddToTodo(e)}/>
-                        {todoList.slice(beginList, endList).map((item,index)=>{
-                            return(
-                                    
-                                    <List.Item key={index} style={{listStyle: 'decimal', textDecoration : item.completed ? 'line-through' : 'none'}} >
-                                        <Checkbox onChange={() => this.handelerTodoChecked(item.id) } checked={item.completed ? true : false }/>
-                                        {item.title}
-                                        <Button onClick={()=> this.handlerDeleteTodo(item.id) }>Удалить</Button>
-                                    </List.Item>
-                            )
-                        })}
-                    </List>
-                    <Pagination defaultCurrent={curentPage} defaultPageSize={perPage} total={todoList.length} onChange={(page, pageSize) => this.handlerChangePagination(page, pageSize)}/>
-                </Col>
-            </>
-        )
+    if (list !== undefined) {
+        let showList = (props.uid !== undefined) ? list.filter((item) => item.userId == props.uid) : list;
+        let firstItem = (currentPage - 1) * perPage;
+        let lastItem = currentPage * perPage;
+        let pageList = showList.slice(firstItem, lastItem);
+    
+        return (
+        <>
+            <Col span={8} style={{margin: '0 auto'}}>
+                <h3>Todo list {props.uid ? '#' + props.uid : ''}</h3>
+                <List>
+                    <Input placeholder='Добавить todo в список' onPressEnter={(e) => handlerAddToTodo(e)}/>
+                    {pageList.map((item, index)=>{
+                        return(          
+                            <List.Item key={index} style={{listStyle: 'decimal', textDecoration : item.completed ? 'line-through' : 'none', display: 'flex'}} >
+                                <div>
+                                    <Checkbox onChange={() => handelerTodoChecked(item.id) } checked={item.completed ? true : false } style={{marginRight: 16}}/>
+                                    <span>{item.title}</span>
+                                </div>
+                                <Button className={classes.button} onClick={()=> handlerDeleteTodo(item.id) }>Удалить</Button>
+                            </List.Item>
+                        )
+                    })}
+                </List>
+                <Pagination size="small" showSizeChanger showQuickJumper defaultCurrent={currentPage} defaultPageSize={perPage} total={showList.length} onChange={handlerUpdatePages} style={{paddingBottom: 16}} />
+            </Col>
+        </>
+        );
     }
+    else {
+        return(<></>);
+    }
+    
 }
+
+export default TodoList;
